800a422b3b8c7c48bbf407aee8ad03f838610e1d
karmyshunde-sudo <karmy.shunde@gmail.com>
2025-10-14 13:38:34 +0800

    Update git_utils.py

--- utils/git_utils.py
+++ utils/git_utils.py
@@ -20,11 +20,11 @@ import base64
 
 # 初始化日志
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-handler = logging.StreamHandler()
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+# logger.setLevel(logging.INFO)
+# handler = logging.StreamHandler()
+# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# handler.setFormatter(formatter)
+# logger.addHandler(handler)
 
 # 文件计数器和线程锁
 _file_count = 0

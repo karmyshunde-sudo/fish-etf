d1c4ccc6642c6fd7e289e487d6464d23496c93cc
karmyshunde-sudo <karmy.shunde@gmail.com>
2025-10-14 13:39:37 +0800

    Update index_yesno.py

--- index_yesno.py
+++ index_yesno.py
@@ -19,11 +19,11 @@ from wechat_push.push import send_wechat_message
 
 # 初始化日志
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-handler = logging.StreamHandler()
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+# logger.setLevel(logging.INFO)
+# handler = logging.StreamHandler()
+# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# handler.setFormatter(formatter)
+# logger.addHandler(handler)
 
 # 指定计算的指数列表（硬编码，包含完整策略信息）
 INDICES = [
